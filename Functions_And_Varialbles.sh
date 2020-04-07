5e655bcf14b3f6f42edf995b3444711bcb7f21a7
Roshan Munavar <roshan.ahtina@gmail.com>
2020-04-07 13:26:29 +0530

    Update Functions_And_Varialbles.sh

--- Functions_And_Varialbles.sh
+++ Functions_And_Varialbles.sh
@@ -48,6 +48,4 @@ fi
 if [ "$1" == "help" ]; then
     echo "====We have got the help option so getting the help content as below====="
     help
-    else
-    echo "==HELP content is done....!==="
 fi
