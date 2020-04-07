25e1a56f3cd5821be7a6b852b7c06d3d34a851f8
Roshan Munavar <roshan.ahtina@gmail.com>
2020-04-07 13:15:28 +0530

    Update Functions_And_Varialbles.sh

--- Functions_And_Varialbles.sh
+++ Functions_And_Varialbles.sh
@@ -5,49 +5,49 @@ set -e
 # This function block is for update and cleanup the packages os level.....! 
 update ()
 {
-    echo "We are starting the OS update by using you update all command....!"
-    yum update all -yy
+    echo "--------------We are starting the OS update by using you update all command....!-----------------"
+    yum update -yy
 }
 
 cleanup ()
 {
-      echo "We are cleaning up after yum update for OS"    
-      yum  clean all -yy
+      echo "---------------We are cleaning up after yum update for OS--------------"    
+      yum  clean -yy
 }
 
 help ()
 {
-    echo "Please use following prameters as usage 1) update or 2) cleanup"
-    echo "Please use correct prameters for script to run..."
+    echo "----Please use following prameters as usage (1) update or (2) cleanup----"
+    echo "----Please use correct prameters for script to run...-----"
 }
 
 # If 1st parameters is UPDATE and then execute as below
 if [ "$1" == "update" ]; then
-    echo "We are got 1st parameter as 'update', so we are about to update the OS"
+    echo "===============We are got 1st parameter as 'update', so we are about to update the OS============"
     update
     sleep 2
-    echo "UPDATE has been done"
+    echo "=============UPDATE has been done============="
 fi
 
 # If 1st parameters is cleanup and then execute as below
 if [ "$1" == "cleanup" ]; then
-    echo "We are anout to cleanup the OS"
+    echo "==============We are anout to cleanup the OS=================="
     cleanup
     sleep 2
-    echo "CLEANUP has been done"
+    echo "=================CLEANUP has been done=================="
 fi
 
 # What if we didn't get update and cleanup option as 1st prameters
 if [ "$1" != "update" -o "cleanup" ]; then
-   echo "Plesae use update or clenaup to perform the action on line server...."
+   echo "==================Plesae use update or clenaup to perform the action on line server======================="
    else
-   echo "Else get rid of using this script... Thanks"
+   echo "==============Else get rid of using this script... Thanks==================="
 fi
 
 # Please below is the help option for more usage of the script
 if [ "$1" == "help" ]; then
-    echo "We have got the help option so getting the help content as below"
+    echo "===============We have got the help option so getting the help content as below================="
     help
     else
-    echo "HELP content is done....!"
+    echo "=============HELP content is done....!==============="
 fi
