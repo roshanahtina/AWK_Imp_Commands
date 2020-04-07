292451352ab3c3cac5a318f6df64d2c42ac30a89
Roshan Munavar <roshan.ahtina@gmail.com>
2020-04-07 12:09:44 +0530

    Update Functions_And_Varialbles.sh

--- Functions_And_Varialbles.sh
+++ Functions_And_Varialbles.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# This function block is for update the packages 
+update ()
+{
+    echo "We are starting the OS update by using you update all command....!"
+    yum update all -yy
+
+}
+
+cleanup ()
+{
+      echo "We are cleaning up after yum update for OS"    
+      yum  clean all -yy
+
+
+}
