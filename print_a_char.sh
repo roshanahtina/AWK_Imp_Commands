4385fa7adb2f8b2e7173d27b443f15f301b4538b
Roshan Munavar <roshan.ahtina@gmail.com>
2020-04-26 15:01:46 +0530

    Update print_a_char.sh

--- print_a_char.sh
+++ print_a_char.sh
@@ -7,9 +7,17 @@ set -e
 char=$1
 count=$2
 
+forloopfunc ()
+{
 for (( i=0; i<=$count; i++ ))
 do
 echo "This is My Charector - $char..."
 done
 
 exit
+}
+
+#Calling the function to print the char
+echo -e "This is the function using repeatedly print a char \n And below are the result"
+forloopfunc
+
