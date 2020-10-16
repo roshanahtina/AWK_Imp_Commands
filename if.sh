4f14e473117ea0ff88c7527e5d6a12bfa79ffc23
roshanahtina <roshan.ahtina@gmail.com>
2020-10-16 15:20:20 +0000

    Added a if condition

---
+++ if.sh
@@ -0,0 +1,7 @@
+ # Please below is the help option for more usage of the script
+ if [ "$1" == "help" ]; then
+     echo "We have got the help option so getting the help content as below"
+   #  help
+    else
+     echo "HELP content is done....!"
+ fi
