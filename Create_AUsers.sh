03a6e721ee6a50d966142f32b9009572f904cf49
Roshan Munavar <roshan.ahtina@gmail.com>
2020-12-26 09:27:58 +0530

    Create Create_AUsers.sh

---
+++ Create_AUsers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Enter the a User list file as $1.
+USERFILE=$1
+
+if [ "USERFILE" = "" ]
+  then
+  echo "Please provide a valide user file"
+exit 10
+
+elif test -e $USERFILE
+then
+  for user in `cat $USERFILE`
+    do
+      useradd $user; echo "$user:linux" | chpasswd
+      echo $user
+    done
+exit 20
+
+else
+  echo "Provide a valide input file"
+
+fi
