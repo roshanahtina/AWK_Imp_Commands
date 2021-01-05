c31f7f21606c638cfd1b247da6e5631480e0b323
Roshan Munavar <roshan.ahtina@gmail.com>
2021-01-05 09:06:31 +0530

    Update Replacing_AWord.sh

--- Replacing_AWord.sh
+++ Replacing_AWord.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for host in `ls <Path to databags>`
+for host in `ls <Path to file names>`
 do
 # Echo the hostname where it's making the chages from the file name.
 echo $host
 hostname=`echo "$host" | cut -d '.' -f 1`
 #echo $hostname
-sed -i "" "2s/\"kickstart\"/\"${hostname}\"/" <Path to databags>"${hostname}".json
-sed -i "" '153,181s/\"unixadmins\"/\"null\"/' <Path to databags>"${hostname}".json
+sed -i "" "2s/\"kickstart\"/\"${hostname}\"/" <Path to file names>"${hostname}".json
+sed -i "" '153,181s/\"unixadmins\"/\"null\"/' <Path to file names>"${hostname}".json
 done
